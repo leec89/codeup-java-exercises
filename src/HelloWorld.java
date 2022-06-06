bf45370f85bb435ad4a7a44f32ee926dea11a353
leec89 <leec89@gmail.com>
2022-06-06 13:28:56 -0500

    feat: java syntax lecture and exercise d43w11

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,26 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
+
+        int weight;
+        String breed = "Golden Retriever";
+
+        weight = 40;
+        breed  = "Puppy";
+
+        long longNum;
+
+        longNum = 123L;
+
+        System.out.println(weight);
+
+        System.out.println("Sherman is a " + breed);
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
---
+++ src/JavaSyntaxLecture.java
@@ -0,0 +1,16 @@
+public class JavaSyntaxLecture {
+
+    public static void main(String[] args) {
+
+        int weight;
+        String breed = "Golden Retriever";
+
+//        weight = "forty";
+        breed  = "Puppy";
+
+//        System.out.println(weight);
+
+        System.out.println("Sherman is a " + breed);
+    }
+
+}
